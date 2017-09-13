88fd51c62b7ca8a95d037f48013538d3cf43c35e
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 14:22:04 -0400

    Updated from Brython Server: 9/13/2017 2:22:03 PM

--- unitConverter.py
+++ unitConverter.py
@@ -7,7 +7,24 @@ print("2) Kilgrams to pounds")
 print("3) Liters to gallons")
 print("4) Celsuis to Fahrenheit")
 
-number = int(input("Choose a number 1-4: "))
+number = int(input("Choose a number: "))
+
 if number == 1:
-    temp = float(input("Enter degrees in Celsuis: "))
+    distKM = float(input("Enter distance in kilometers: "))
+    distMI = distKM * 0.621371
+    print(distKM, "kilometers is", distMI, "miles")
+
+elif number == 2:
+    massKG = float(input("Enter a mass in kilograms: "))
+    massLBS = massKG * 2.20462
+    print(massKG, "kilograms is", massLBS, "pounds")
+elif number == 3:
+    volumeL = float(input("Enter a volume in liters: "))
+    volumeGal = volumeL * 0.264172
+    print(volumeL, "liters is", volumeGal, "gallons")
+else:
+    tempC = float(input("Enter a temperature in Celsius: "))
+    tempF = tempC * 1.8 + 32
+    print(tempC, "degrees Celsius is", tempF, "degrees Fahrenheit")
+
     
