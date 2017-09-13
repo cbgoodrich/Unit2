54d5b8b3d53b5ce04808de738b693faf3928a664
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 13:39:27 -0400

    Updated from Brython Server: 9/13/2017 1:39:26 PM

--- ifDemo.py
+++ ifDemo.py
@@ -1,1 +1,15 @@
+#Charlie Goodrich
+#09/13/17
+#ifDemo.py - first if/else program
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("Your number is positive my guy!")
+
+elif number < 0:
+    print("Your number is negative")
+    
+else:
+    print("Your number is zero")
 
