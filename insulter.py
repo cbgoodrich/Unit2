94c6f03f1fa5af565596870e9fafa2e23d105cdd
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 14:46:20 -0400

    Updated from Brython Server: 9/13/2017 2:46:20 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,17 @@
+#Charlie Goodrich
+#09/13/17
+#insulter.py - insults you to your core
 
+from random import randint
+
+input("Enter your name: ")
+
+number = randint(1,5)
+if number == 1:
+    print("You eat cereal with a fork")
+elif number == 2:
+    print("You don't know how to commit your files")
+elif number == 3:
+    print("I've owned dogs smarter than you")
+elif number == 4:
+    print("You 
