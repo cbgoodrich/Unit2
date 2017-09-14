66f300ca3c9c429784fbd2a7a7d4ea706add1ff8
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 20:32:21 -0400

    Updated from Brython Server: 9/13/2017 8:32:21 PM

--- unitConverter.py
+++ unitConverter.py
@@ -7,7 +7,7 @@ print("2) Kilgrams to pounds")
 print("3) Liters to gallons")
 print("4) Celsius to Fahrenheit")
 
-number = int(input("Choose a number: "))
+number = int(input("Choose a conversion: "))
 
 if number == 1:
     distKM = float(input("Enter distance in kilometers: "))
