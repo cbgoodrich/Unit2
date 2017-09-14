10cdc446daf73f271a35d44bd7984c93a65b269c
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 20:32:06 -0400

    Updated from Brython Server: 9/13/2017 8:32:06 PM

--- unitConverter.py
+++ unitConverter.py
@@ -5,7 +5,7 @@
 print("1) Kilometers to miles")
 print("2) Kilgrams to pounds")
 print("3) Liters to gallons")
-print("4) Celsuis to Fahrenheit")
+print("4) Celsius to Fahrenheit")
 
 number = int(input("Choose a number: "))
 
