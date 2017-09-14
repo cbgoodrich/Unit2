13db7d6ba067cefd7d9562220a37640097049028
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 20:41:11 -0400

    Updated from Brython Server: 9/13/2017 8:41:10 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -4,14 +4,26 @@
 
 grade = int(input("What is your grade? "))
 
-if grade <= 100 and grade >=90:
+if grade <= 100 and grade >=93:
     print("You have an A")
-elif grade < 90 and grade >= 80:
+elif grade < 93 and grade >= 90:
+    print("You have an A-")
+elif grade < 90 and grade >= 87:
+    print("You have a B+")
+elif grade < 87 and grade >= 83:
     print("You have a B")
-elif grade < 80 and grade >=70:
+elif grade < 83 and grade >= 80:
+    print("You have a B-")
+elif grade < 80 and grade >= 77:
+    print("You have a C+")
+elif grade < 77 and grade >= 73:
     print("You have a C")
-elif grade < 70 and grade >= 60:
+elif grade < 73 and grade >= 70:
+    print("You have a C-")
+elif grade < 70 and grade >= 63:
     print("You have a D")
+elif grade < 63 and grade >= 60:
+    print("You have a D-")
 else:
     print("You're failing :(")
 
