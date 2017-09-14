337b81bfbba4d228fff2084e1b59588254a30554
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-14 14:34:30 -0400

    Updated from Brython Server: 9/14/2017 2:34:29 PM

--- coloredSquare.py
+++ coloredSquare.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#09/14/17
+#coloredSquare.py - pops up a colored square
 
+from ggame import *
+from random import randint
+
+number = randint(1,3) #picks a random number to be assigned to a color
+
+if number == 1:
+    red = Color(0xff0000, 1)
+    line = LineStyle(3, red)
+    rectangle = RectangleAsset(100, 100, line, red)
+    
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
+    
