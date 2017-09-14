4c1b01d2c613b47b114baf71fcbf40c76ec94d79
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-13 20:10:46 -0400

    Updated from Brython Server: 9/13/2017 8:10:45 PM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,13 @@
+#Charlie Goodrich
+#09/13/17
+#coinFlip.py - tells you heads or tails
 
+from random import randint
+
+number = randint(1,2)
+print("Flipping your coin...")
+if number == 2:
+    print("Tails")
+
+else:
+    print("Heads")
