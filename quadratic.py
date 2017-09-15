a25e4c6b3414152077fe5ffdacd94e0f4f349616
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-15 14:54:59 -0400

    Updated from Brython Server: 9/15/2017 2:54:58 PM

--- quadratic.py
+++ quadratic.py
@@ -1,1 +1,8 @@
+#Charlie Goodrich
+#09/15/17
+#quadratic.py - gives you the solution to your quadratic
 
+print("ax^2 + bx + c = 0")
+a = float(input("Enter an a-value: "))
+b = float(input("Enter a b-value: "))
+c = float(input("Enter a c-value: "))
