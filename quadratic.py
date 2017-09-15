96b3336877753a6a58b3d5e31629f454bc8e1d58
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-15 15:19:00 -0400

    Updated from Brython Server: 9/15/2017 3:18:58 PM

--- quadratic.py
+++ quadratic.py
@@ -2,7 +2,20 @@
 #09/15/17
 #quadratic.py - gives you the solution to your quadratic
 
+from math import sqrt
+
 print("ax^2 + bx + c = 0")
-a = float(input("Enter an a-value: "))
-b = float(input("Enter a b-value: "))
-c = float(input("Enter a c-value: "))
+a = float(input("Enter an a-value: ")) #a-value of quadratic
+b = float(input("Enter a b-value: ")) #b-value of quadratic
+c = float(input("Enter a c-value: ")) #c-value of quadratic
+
+if sqrt(b**2 - 4*a*c) >= 0: #testing if the determinate is greater than 0
+    x1 = (-b + (b**2 - (4 * a * c))**.5)/2a
+    x2 = (-b - (b**2 - (4 * a * c))**.5)/2a
+    print("X =", x1)
+    print("X =", x2)
+else:
+    real = (-b)/2a
+    imaginary = (abs(b**2 - 4*a*c)**.5)/2a
+    print("X =", real, "+", imaginary, "i")
+    print("x =", real, "-", imaginary, "i")
