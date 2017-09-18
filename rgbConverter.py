eded34c4e59fe07e495e4be1af19a8a657864620
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-18 14:54:36 -0400

    Updated from Brython Server: 9/18/2017 2:54:35 PM

--- rgbConverter.py
+++ rgbConverter.py
@@ -3,14 +3,9 @@
 #rgbConverter.py - converts rgb to hex
 
 rgb1 = int(input("Enter a number between 0 and 255: "))
-rgb2 = int(input("Enter a number between 0 and 255: "))
-rgb2 = int(input("Enter a number between 0 and 255: "))
 
 dig1 = rgb1&16
 dig2 = (rgb1//16)%16
-dig3 = rgb2&16
-dig4 = (rgb2//16)%16
-dig5 = rgb3&16
-dig6 = (rgb3//16)%16
 
-if dig1 
+if dig1 == 10:
+    dig1 = "A"
