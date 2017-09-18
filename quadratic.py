95ea3a9c30c47659a80299d8ec7c788d73b4ad5f
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-17 20:40:04 -0400

    Updated from Brython Server: 9/17/2017 8:40:03 PM

--- quadratic.py
+++ quadratic.py
@@ -9,13 +9,13 @@ a = float(input("Enter an a-value: ")) #a-value of quadratic
 b = float(input("Enter a b-value: ")) #b-value of quadratic
 c = float(input("Enter a c-value: ")) #c-value of quadratic
 
-if sqrt(b**2 - 4*a*c) >= 0: #testing if the determinate is greater than 0
-    x1 = (-b + sqrt((b**2 - 4*a*c))/2*a
-    x2 = (-b - sqrt((b**2 - 4*a*c))/2*a
+if (b**2 - 4*a*c) >= 0: #testing if the determinate positive
+    x1 = (-b/(2*a)) + (sqrt(b**2 - (4*a*c)))/(2*a)
+    x2 = (-b/(2*a)) - (sqrt(b**2 - (4*a*c)))/(2*a)
     print("X =", x1)
     print("X =", x2)
 else:
-    real = (-b)/2a
-    imaginary = sqrt((abs(b**2 - 4*a*c)))/2*a
+    real = (-b)/(2*a)
+    imaginary = sqrt(abs(b**2 - (4*a*c)))/(2*a)
     print("X =", real, "+", imaginary, "i")
     print("x =", real, "-", imaginary, "i")
