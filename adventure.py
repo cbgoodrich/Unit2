a54eea3219626e285ff8dc080add51967ed67dbb
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-24 19:28:02 -0400

    Updated from Brython Server: 9/24/2017 7:28:01 PM

--- adventure.py
+++ adventure.py
@@ -4,18 +4,18 @@
 
 print("A bear is chasing you")
 print("You come to a river")
-riverYOrN = input("Do you jump in the river? ")
-if riverYOrN == "yes":
+riverYOrN = input("Do you jump in the river? (yes or no) ")
+if riverYOrN == "yes" or riverYOrN == "y":
     print("Thankfully you know how to swim")
     print("However, you notice a nearby shark")
-    sharkYOrN = input("Do you punch it in the nose? ")
-    if sharkYOrN == "yes":
+    sharkYOrN = input("Do you punch it in the nose? (yes or no) ")
+    if sharkYOrN == "yes" or sharkYOrN == "y":
         print("The shark eats you, and you die :(")
     else:
         print("You befriend the shark")
         print("Soon you become hungy, your stomach is rumbling")
-        soupYOrN = input("Do you want to make sharkfin soup? ")
-        if soupYOrN == "yes":
+        soupYOrN = input("Do you want to make sharkfin soup? (yes or no) ")
+        if soupYOrN == "yes" or soupYOrN == "y":
             print("The shark eats you before you can kill it. It's hungry too :(")
         else:
             print("You die of starvation :(")
